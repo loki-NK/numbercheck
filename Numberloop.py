3306cd6a3ce4367c4c4b8972f4e66139a89b165e
KIRAN NK <loki-NK@users.noreply.github.com>
2019-05-22 21:39:00 +0530

    Add files via upload

---
+++ Numberloop.py
@@ -0,0 +1,19 @@
+import csv
+from requests.auth import HTTPBasicAuth
+import requests
+import json
+file = open('Response.txt', 'w+')
+with open('numbercheck.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        URL = "https://apix.aerialink.net/v4/numbers"
+        Param = {'numbers': row[0]}
+        response = requests.get(url = URL,params = Param,auth=HTTPBasicAuth('d6622ccf','64abd6a0'))
+        if (response.status_code >= 401):
+            print("Error")
+        else:
+            a=json.loads(str(response))
+            l = a['aerialink']['transactions']
+            v = l['transaction']['number']['operator']['mcc']['mnc']['operatorType']['wireless']['country']['countryCode']['validNumber']['lastUpdated']
+            file.write(v+'\n'+'\n')
+    file.close()
